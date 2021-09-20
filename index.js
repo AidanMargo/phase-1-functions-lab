649218c030dd1708e9eda9da406299b5ff578320
Aidan Margo <aidanmargo@gmail.com>
2021-09-20 17:29:35 -0500

    commit tree 531c74aa9041da1c90e957e6d2b8639ee35e4763

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+  const distanceInBlocks = Math.abs(street-42);
+  return distanceInBlocks
+}
+
+function distanceFromHqInFeet(street){
+  const blocks = distanceFromHqInBlocks(street)
+  return blocks*264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  const feet = distanceFromHqInFeet();
+  const distanceTravelled = Math.abs((start - end) * 264);
+  return distanceTravelled;
+}
+
+function calculatesFarePrice(start, end){
+  let distance = distanceTravelledInFeet(start, end);
+  let cost;
+
+  if(distance < 400){
+    return 0
+  } else if(distance > 400 && distance < 2000){
+    return (distance - 400)*0.02
+  } else if(distance > 2000 && distance < 2500){
+    return 25
+  } else {
+    return 'cannot travel that far'
+  }
+}
+
+
+distanceFromHqInBlocks()
+distanceFromHqInFeet()
+distanceTravelledInFeet()
+calculatesFarePrice()
